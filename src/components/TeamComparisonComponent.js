968dd09f505fc0de2898e257b75fd61660e41df3
Jon Jackson <jon.jackson@perficient.com>
2024-01-14 12:15:43 -0500

    fixing vertical alignment on mobile

--- src/components/TeamComparisonComponent.js
+++ src/components/TeamComparisonComponent.js
@@ -15,31 +15,34 @@ const TeamComparisonComponent = ({ date, startTime, homeTeamLogo, awayTeamLogo, 
         {/* Team 1 */ }
         <Grid item xs={ 6 } container direction="column" alignItems="center">
           <img src={ homeTeamLogo } alt={ `${homeTeam} Logo` } style={ { maxWidth: '50%', height: 'auto' } } />
-          <Typography variant="h5" gutterBottom style={ { textAlign: 'center' } }>
-            { homeTeam }
-          </Typography>
-          {/* Display the score for Team 1 */ }
-          <Typography className="team-score" variant="h6" gutterBottom style={ { textAlign: 'center' } }>
-            { homeTeamScore }
-          </Typography>
+          <div className="team-info">
+            <Typography variant="h5" gutterBottom style={ { textAlign: 'center' } }>
+              { homeTeam }
+            </Typography>
+            {/* Display the score for Team 1 */ }
+            <Typography className="team-score" variant="h6" gutterBottom style={ { textAlign: 'center' } }>
+              { homeTeamScore }
+            </Typography>
+          </div>
         </Grid>
 
         {/* Team 2 */ }
         <Grid item xs={ 6 } container direction="column" alignItems="center">
           <img src={ awayTeamLogo } alt={ `${awayTeam} Logo` } style={ { maxWidth: '50%', height: 'auto' } } />
-          <Typography variant="h5" gutterBottom style={ { textAlign: 'center' } }>
-            { awayTeam }
-          </Typography>
-          {/* Display the score for Team 2 */ }
-          <Typography className="team-score" variant="h6" gutterBottom style={ { textAlign: 'center' } }>
-            { awayTeamScore }
-          </Typography>
+          <div className="team-info">
+            <Typography variant="h5" gutterBottom style={ { textAlign: 'center' } }>
+              { awayTeam }
+            </Typography>
+            {/* Display the score for Team 2 */ }
+            <Typography className="team-score" variant="h6" gutterBottom style={ { textAlign: 'center' } }>
+              { awayTeamScore }
+            </Typography>
+          </div>
         </Grid>
       </Grid>
 
       {/* Stats */ }
       <div>
-        {/* <p>{ date === todaysDate ? 'Today at ' : date } { easternTime } ET | { pacificTime } PT</p> */ }
         <p>{ easternTime } ET | { pacificTime } PT</p>
         { Object.entries(stats).map(([key, value]) => (
           <Typography key={ key } variant="body2" color="textSecondary" gutterBottom>
@@ -49,6 +52,9 @@ const TeamComparisonComponent = ({ date, startTime, homeTeamLogo, awayTeamLogo, 
       </div>
     </div>
   );
+
+
+
 };
 
 export default TeamComparisonComponent;
