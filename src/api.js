fb029914db10140e02dccff8508cd349ea3b273a
Jon Jackson <jon.jackson@perficient.com>
2024-01-16 14:48:12 -0500

    working locally

--- src/api.js
+++ src/api.js
@@ -52,8 +52,8 @@
 import axios from 'axios';
 import { getTodaysDate } from './util/dates';
 
-// const API_URL = '/api/v1'; // Change to the proxy path
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = '/api/v1'; // Change to the proxy path
+// const API_URL = process.env.REACT_APP_API_URL;
 console.log({ API_URL });
 const TEAM_DETAILS_URL = 'https://api.nhle.com';
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,14 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function (app) {
-//     app.use(
-//         '/api',
-//         createProxyMiddleware({
-//             target: process.env.REACT_APP_API_URL,
-//             changeOrigin: true,
-//             pathRewrite: {
-//                 '^/api': '/',
-//             },
-//         })
-//     );
-// };
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'https://api-web.nhle.com',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': '/',
+            },
+        })
+    );
+};
